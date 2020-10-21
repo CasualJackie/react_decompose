c41e0e94465b857f2391f5e8d4734bdd0fa21b12
Evgeniy <more.inspirationn@gmail.com>
2020-10-21 12:15:22 +0300

    Updates

---
+++ precache-manifest.615b466206c6e2934ea1aea2042358ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac08af408c894af76525c667a51b5cb3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5331ce514080dc2fb9df",
+    "url": "./static/css/main.ef451813.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5331ce514080dc2fb9df",
+    "url": "./static/js/main.b59eaef4.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
